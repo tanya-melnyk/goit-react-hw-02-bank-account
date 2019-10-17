d6852a49d27146342856cacf5120f0f42e640c0e
Tetiana Melnyk <prostotayna@gmail.com>
2019-10-17 09:48:11 +0300

    Updates

---
+++ precache-manifest.8c1616c8d024abfd5653215c3b8ce5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c0e508f6e89e135d2d8c90aa77a917",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "4b206772c0608d4b0a63",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4b206772c0608d4b0a63",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.ffcc1207.chunk.js"
+  },
+  {
+    "revision": "5ef845703d392603b288",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.fbc0620b.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
